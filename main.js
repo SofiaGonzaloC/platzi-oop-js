afe012c25fcbf19d850b2268dcd6c13e3295bb3f
Sofia Gonzalez <sofiagonzaloc@outlook.com>
2021-10-11 11:22:55 -0600

    Formas de crear un objeto y su instancia

--- main.js
+++ main.js
@@ -11,18 +11,64 @@ const natalia = {
     }
 }
 
-function student(name, age, cursosAprobados){
+console.group("Prototipo con la sintaxis de función")
+function student(name, age, cursosAprobados) {
     this.name = name
     this.age = age
     this.cursosAprobados = cursosAprobados
 }
-
-student.prototype.aprobarCurso = function(curso) { // Añade la función aprobarCurso al objeto prototipo student
+student.prototype.aprobarCurso = function (curso) { // Añade la función aprobarCurso al objeto prototipo student
     this.cursosAprobados.push(curso)
 }
-
+// Instancia
 const juanita = new student(
     "Juanita Alejandra",
     15,
     ["POO", "Introducción a videojuegos", "Introducción a diseño"]
 )
+console.groupEnd()
+
+console.group("Prototipos con la sintaxis de clases")
+class student2 {
+    constructor(name, age, cursosAprobados) { // metodo que se ejecuta
+        this.name = name
+        this.age = age
+        this.cursosAprobados = cursosAprobados
+    }
+
+    aprobarCurso(nuevoCursito) {
+        this.cursosAprobados.push(nuevoCursito)
+    }
+}
+// Instancia
+const miguel = new student2(
+    "Miguel Álvarez",
+    25,
+    [
+        "Análisis de negocios",
+        "Principios de visualización de datos"
+    ]
+)
+console.groupEnd()
+
+console.group("Recibir UN solo parámetro que sea de tipo objeto")
+class student3 {
+    constructor({ name, age, cursosAprobados}) {
+        this.name = name
+        this.age = age
+        this.cursosAprobados = cursosAprobados
+    }
+
+    aprobarCurso(nuevoCursito) {
+        this.cursosAprobados.push(nuevoCursito)
+    }
+}
+//Instancia
+const jose = new  student3({
+    name: "Jose",
+    age: 28,
+    cursosAprobados: [
+        "ES2016",
+        "Creación de videojuegos"
+    ]
+})
