913582c5b35d5cc2a19f42e6f264fdb7300bbdaf
Sofia Gonzalez <sofiagonzaloc@outlook.com>
2021-10-11 12:43:01 -0600

    students.js file created, object created there

---
+++ student.js
@@ -0,0 +1,23 @@
+class student{
+    constructor({
+        name,
+        email,
+        username,
+        twitter = undefined,
+        instagram = undefined,
+        facebook = undefined,
+        points = undefined,
+        cursosAprobados = [],
+    }){
+        this.name = name;
+        this.email = email;
+        this.username = username;
+        this.socialMedia = {
+            twitter,
+            instagram,
+            facebook
+        },
+        this.points = points;
+        this.cursosAprobados = cursosAprobados;
+    }
+}
