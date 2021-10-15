006d7bc76918a726e7ea3f3884c6a914dfcde62d
Sofia Gonzalez <sofiagonzaloc@outlook.com>
2021-10-15 12:06:48 -0600

    added classes for 3 types of students using inheritance

--- objects/student.mjs
+++ objects/student.mjs
@@ -1,4 +1,4 @@
-export class student{
+export class Student {
     constructor({
         name,
         email,
@@ -8,7 +8,7 @@ export class student{
         facebook = undefined,
         points = undefined,
         cursosAprobados = [],
-    }){
+    }) {
         this.name = name;
         this.email = email;
         this.username = username;
@@ -17,7 +17,45 @@ export class student{
             instagram,
             facebook
         },
-        this.points = points;
+            this.points = points;
         this.cursosAprobados = cursosAprobados;
+    }
+}
+
+class FreeStudent extends Student {
+    constructor(props) {
+        super(props); // Permite referenciar al constructor de la clase madre
+    }
+
+    approveCourse(newCourse) {
+        if (newCourse.isFree) { // Valida que el curso sea gratis
+            this.cursosAprobados.push(newCourse)
+        } else {
+            console.warn("Lo siento " + this.name + "este curso no es parte del plan free")
+        }
+    }
+}
+
+class BasicStudent extends Student {
+    constructor(props) {
+        super(props); // Permite referenciar al constructor de la clase madre
+    }
+
+    approveCourse(newCourse) {
+        if (newCourse.lang !== "english") { // Valida que el curso no este en ingles
+            this.cursosAprobados.push(newCourse)
+        } else {
+            console.warn("Lo siento " + this.name + "este curso no es parte del plan basic")
+        }
+    }
+}
+
+class ExpertStudent extends Student {
+    constructor(props) {
+        super(props); // Permite referenciar al constructor de la clase madre
+    }
+
+    approveCourse(newCourse) {
+        this.cursosAprobados.push(newCourse)
     }
 }
